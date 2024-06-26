43cd94831139c9895b06dd23382782a6930ea2e9
avinashverma <avinashverma2908@gmail.com>
2024-06-27 01:19:13 +0530

    first commit

---
+++ src/main/java/org/avinash/FirstProjectApplication.java
@@ -0,0 +1,13 @@
+package org.avinash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FirstProjectApplication.class, args);
+    }
+
+}
